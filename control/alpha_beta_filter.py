e849437b66bed48a4307d74cdfea81bb501f97e5
AyonRRahman <raqibur.ayon@gmail.com>
2023-03-30 22:12:08 +0200

    MARS30

--- control/alpha_beta_filter.py
+++ control/alpha_beta_filter.py
@@ -2,16 +2,28 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+df = pd.read_csv('_slash_br5_slash_mavros_slash_imu_slash_water_pressure.csv')
+
+def my_operation(x):
+    return (x - 101300)/(1000*9.80665)
+
+df['fluid_pressure'] = df['fluid_pressure'].apply(my_operation)
+z = df['fluid_pressure'].to_list()
+
+
 dt = 0.001
 
-xk_0=0
+xk_0=z[0]
 vk_0 = 0
 a = 0.45
 b = 0.1
-t = np.linspace(0,10,10000)
+# t = np.linspace(0,10,10000)
 xk_1 = []
 vk_1 = []
-xm_0 = -10*np.sin(t)
+xm_0 = z
 
 for xm in xm_0:
     # xm = random.random()*100
@@ -29,11 +41,12 @@ for xm in xm_0:
     xk_1.append(xk_0)
     vk_1.append(vk_0)
 
-plt.plot(t, xk_1, label='xk')
-plt.plot(t, vk_1, label ='vk')
-plt.plot(t, xm_0,label='xm_0')
+plt.plot( xk_1, label='xk')
+plt.plot( vk_1, label ='vk')
+plt.plot( xm_0,label='xm_0')
 
 plt.axhline(0, color='red')
+
 plt.axvline(0, color='red')
 
 plt.legend()
